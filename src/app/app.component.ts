ac53d3b7e7ca6860a7fb8ec443e9e7886024b726
Vitaliy Mamontov <vintixfish@gmail.com>
2020-10-27 22:18:52 +0200

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Initial';
-
-  OnInput(event: any) {
-    this.title = event.target.value;
-  }
+  backgroundToggle = false;
 }
