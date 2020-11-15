639834a7c70e170475a6b69b2cc4757b69fe2c4f
Vitaliy Mamontov <vintixfish@gmail.com>
2020-11-15 22:13:03 +0200

    Added own pipe using Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {MultByPipe} from './Pipes/mult-by.pipe';
+import { ExMarksPipe } from './Pipes/ex-marks.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe
+    MultByPipe,
+    ExMarksPipe
   ],
   imports: [
     BrowserModule,
