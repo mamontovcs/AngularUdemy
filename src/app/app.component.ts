02e78f0432b9167972e553f125e2e1f50b5f56a9
Vitaliy Mamontov <vintixfish@gmail.com>
2020-11-15 21:23:47 +0200

    Cleared project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  isVisible = true;
-
-  posts: Post[] = [
-    {title: 'Мой первый пост', text: 'Я все еще учу Ангуляр', id: 3123123},
-    {title: 'Мой второй пост', text: 'Я хочу быть фулл стэком', id: 345678},
-  ];
-
-  ngOnInit(): void {
-    setTimeout(() => {
-        console.log('TimeOut');
-        this.posts[0] = {
-          title: 'Changed',
-          text: 'changed 2',
-          id: 147
-        };
-      }
-      , 5000);
-  }
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-  }
-
-  removePost(id: number) {
-    console.log(id);
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {PostFormComponent} from './post-form/post-form.component';
-import {PostComponent} from './post/post.component';
-import {StyleDirective} from './Directives/style.directive';
-import { IfNotDirective } from './Directives/if-not.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostFormComponent,
-    PostComponent,
-    StyleDirective,
-    IfNotDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
