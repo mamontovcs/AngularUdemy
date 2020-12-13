42e852ac0cd2d1bd5f6886da52c1c0b2d808c642
Vitaliy Mamontov <vintixfish@gmail.com>
2020-12-13 14:48:21 +0200

    Inejected one service to another one

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,20 +1,21 @@
 import {Injectable} from '@angular/core';
+import {LogService} from './log.service';
 
-@Injectable({
-
-})
+@Injectable({})
 export class CounterService {
 
-  constructor() {
+  constructor(private logger: LogService) {
   }
 
   counter = 0;
 
   increase() {
+    console.log('Increased');
     this.counter++;
   }
 
   decrease() {
+    console.log('Decreased');
     this.counter--;
   }
 }
